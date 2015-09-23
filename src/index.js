1ae490f0dc21195fef514dc2a685c04373d0e8bf
phi <phi1618jp@gmail.com>
2015-09-24 01:44:59 +0900

    update

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var app = express();
+var ip = require('ip');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,7 +28,8 @@ app.use(logger('dev'));
 // });
 
 var server = app.listen((process.env.PORT || 3000), function() {
-  var host = server.address().address;
+  // var host = server.address().address;
+  var host = ip.address();
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
